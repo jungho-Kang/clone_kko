ba04dfb8f53c11c1c9af981f8fb7115b81ef607e
jungho-Kang <rkdwjdgh08@gmail.com>
2024-11-18 18:11:50 +0900

    [jQuery 진행중]

--- js/header.js
+++ js/header.js
@@ -1,90 +1,52 @@
-// 협업에서는 소스 컨벤션 중요
-window.addEventListener("load", function () {
-  /*
-    1. 사용자가 스크롤바로 화면아래로 이동시
-    2. header 클래스 하단에 라인을 생성
-    3. 사용자가 스크롤바로 화면 최상단으로 이동시
-    4. header 클래스에 하단에 라인제거
-    */
-  //   header를 보관함에 담아둔다.
-  const header = document.querySelector(".header");
-  //   console.log(header);
+// jQuery의 목적은 2가지
+// html 및 css 제어
+// 외부데이터 연동
 
-  //   스크롤 체크하기
-  window.addEventListener("scroll", function () {
-    const scrollPositionY = window.scrollY;
-    // console.log(scrollPositionY);
-    // header에 class 추가하기 -> <div class = "header header-active"></div>가 된다.
+// html과 css가 화면에 보일 준비 끝나면
+// image, font, mp3, mp4는 로딩 체크를 못함.
+// window.addEventListener("DOMContentLoaded", function () {} );
+$(document).ready(function () {
+  // header를 보관함에 담아둔다.
+  var header = $(".header");
+  // 스크롤 체크하기
+  $(window).on("scroll", function () {
+    var scrollPositionY = $(window).scrollTop();
     if (scrollPositionY > 0) {
-      header.classList.add("header-active");
+      header.addClass("header-active");
     } else {
-      header.classList.remove("header-active");
+      header.removeClass("header-active");
     }
   });
 });
 
 // 이미지 바꾸기 및 메뉴 펼침
-window.addEventListener("load", function () {
-  /**
-   * 아이콘 이미지 바꾸기
-   * <img src="경로" alt="" />
-   * 클릭시 이미지 바꾸기 (토글)
-   */
+$(document).ready(function () {
+  var mobileButton = $("#mb-menu-bt");
+  var mobileButtonImage = $("#mb-menu-bt img");
+  var openIcon = "./images/icon/icon-hbr.png";
+  var closeIcon = "./images/icon/icon-close.png";
+  var mobileMenuBg = $(".bg-mb-menu");
+  var mobileMenu = $(".mb-menu");
 
-  // 1. 버튼 역할하는 ID 찾기
-  const mobileButton = document.querySelector("#mb-menu-bt");
-  // console.log(mobileButton);
-
-  // 2. 버튼 안에 있는 이미지를 찾기
-  const mobileButtonImage = document.querySelector("#mb-menu-bt img");
-  // console.log(mobileButtonImage);
-
-  // 3. 버튼 안에 있는 이미지의 src는 무엇일까?
-  // const srcString = mobileButtonImage.getAttribute("src");
-  // console.log(srcString);
-
-  // 4. 버튼 클릭 처리
-  const openIcon = "./images/icon/icon-hbr.png";
-  const closeIcon = "./images/icon/icon-close.png";
-
-  mobileButton.addEventListener("click", function () {
-    // 이미지의 src에 담겨진 글자를 수정하겠다.
-    const imageSrc = mobileButtonImage.getAttribute("src");
+  mobileButton.on("click", function () {
+    var imageSrc = mobileButtonImage.attr("src");
     if (imageSrc == openIcon) {
-      // 이미지 src를 교체하겠다.
-      mobileButtonImage.setAttribute("src", closeIcon);
-      mobileMenuBg.classList.add("bg-mb-menu-active");
-      mobileMenu.classList.add("mb-menu-active");
+      mobileButtonImage.attr("src", closeIcon);
+      mobileMenuBg.addClass("bg-mb-menu-active");
+      mobileMenu.addClass("mb-menu-active");
     } else {
-      // 이미지 src를 교체하겠다.
-      mobileButtonImage.setAttribute("src", openIcon);
-      mobileMenuBg.classList.remove("bg-mb-menu-active");
-      mobileMenu.classList.remove("mb-menu-active");
+      mobileButtonImage.attr("src", openIcon);
+      mobileMenuBg.removeClass("bg-mb-menu-active");
+      mobileMenu.removeClass("mb-menu-active");
     }
   });
 
-  // 모바일 메뉴 관련 내용을 찾아서 이름을 주고 보관하자.
-  // 1. 모바일 배경
-  const mobileMenuBg = document.querySelector(".bg-mb-menu");
-  // console.log(mobileMenuBg);
-
-  // 2. 모바일 메뉴
-  const mobileMenu = document.querySelector(".mb-menu");
-  // console.log(mobileMenu);
-
-  // 반응형 테스트
-  // PC버전에서는 모두 다 즉, 버튼도 초기화, 모바일 메뉴도 초기화
-  window.addEventListener("resize", function () {
-    // 웹브라우저의 너비를 체크한다.
-    const windowWidth = window.innerWidth;
-    // console.log(windowWidth);
-
-    // css 코드에 반응형 작동이 1024px부터 작동하니까.
+  $(window).on("resize", function () {
+    var windowWidth = $(window).width();
     if (windowWidth > 1024) {
-      // 모바일 메뉴 버튼을 원래대로 되돌린다.
-      mobileButtonImage.setAttribute("src", openIcon);
-      mobileMenuBg.classList.remove("bg-mb-menu-active");
-      mobileMenu.classList.remove("mb-menu-active");
+      mobileButtonImage.attr("src", openIcon);
+      mobileMenuBg.removeClass("bg-mb-menu-active");
+      mobileMenu.removeClass("mb-menu-active");
     }
   });
 });
--- js/slide-logo.js
+++ js/slide-logo.js
@@ -1,18 +1,59 @@
-window.addEventListener("load", function () {
-  // MockData
-  // { imgUrl: "경로", desc: "설명문" }
-  // [ {}, {}, {} ]
-  // api 주소 : json 주소가 어디니?
-  const LOGO_DATA_URL = "/apis/logodata.json";
-  // API를 통한 데이터 불러오기()
-  // ---- request: 리퀘스트
-  // API를 통해 불러들여진 결과물
-  // ---- response : 리스판스
+$(document).ready(function () {
+  var LOGO_DATA_URL = "/apis/logodata.json";
+  $.ajax({
+    url: LOGO_DATA_URL,
+    method: "GET",
+    datatype: "json",
+    success: function (result) {
+      var logoHtml = "";
+      for (var i = 0; i < result.length; i++) {
+        var data = "";
+        data += "<div class='swiper-slide'>";
+        data += "<img src='";
+        data += "/images/etc/";
+        data += result[i].imgUrl;
+        data += "' alt='";
+        data += result[i].desc;
+        data += "' />";
+        data += "</div>";
+        logoHtml += data;
+      }
+
+      var headerLogoTag = $(".header-logo-motion .swiper-wrapper");
+      headerLogoTag.html(logoHtml);
+      // 4. swiper 생성 및 실행
+      var headerLogo = new Swiper(".header-logo-motion", {
+        loop: true,
+        autoplay: {
+          delay: 1500,
+          disableOnInteraction: false,
+        },
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true,
+        },
+      });
+
+      // 추가 1 : 먼저 멈춘다.
+      headerLogo.autoplay.stop();
+      // 추가 2 : 마우스 오버되면 다시 플레이
+      headerLogoTag.addEventListener("mouseenter", function () {
+        headerLogo.autoplay.start();
+      });
+      // 추가 3 : 마우스 아웃되면 멈춤 및 첫 슬라이드로 이동
+      headerLogoTag.addEventListener("mouseleave", function () {
+        headerLogo.autoplay.stop();
+        headerLogo.slideToLoop(0, 500, false); // 무조건 첫 슬라이드로 가라.
+      });
+    },
+    error: function (error) {},
+  });
+
   fetch(LOGO_DATA_URL)
     .then(function (response) {
-      //   console.log(response);
+      // console.log(response);
       const result = response.json();
-      //   console.log(result);
+      // console.log(result);
       return result;
     })
     .then(function (result) {
@@ -62,5 +103,5 @@ window.addEventListener("load", function () {
     .catch(function (error) {
       // console.log(error);
     });
-  //   const logoData;
+  // const logoData;
 });
